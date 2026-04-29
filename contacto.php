ccf64575678d10be6a16312b985d341989c4a182
Carlarriagadaorellana2402 <carriagadaore@gmail.com>
2026-04-28 21:52:28 -0400

    Delete contacto.php

--- contacto.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="../controllers/ContactoController.php">
-<input name="nombre" placeholder="Nombre">
-<input name="email" placeholder="Email">
-<textarea name="mensaje"></textarea>
-<button>Enviar</button>
-</form>
