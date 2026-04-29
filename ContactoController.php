50abdeabdbf5f9a1e94476540d4599743afe705b
Carlarriagadaorellana2402 <carriagadaore@gmail.com>
2026-04-28 21:52:00 -0400

    Delete ContactoController.php

--- ContactoController.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../models/Contacto.php");
-if($_POST){
-    $contacto = new Contacto($_POST['nombre'], $_POST['email'], $_POST['mensaje']);
-    echo $contacto->enviar();
-}
-?>
