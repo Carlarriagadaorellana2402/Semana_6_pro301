b0f9e3b9857a0b7de1a33f275e995bd5bcac0f95
Carlarriagadaorellana2402 <carriagadaore@gmail.com>
2026-04-28 21:52:19 -0400

    Delete UsuarioController.php

--- UsuarioController.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../models/Usuario.php");
-if($_POST){
-    $usuario = new Usuario($_POST['nombre'], $_POST['email'], $_POST['password']);
-    echo $usuario->registrar();
-}
-?>
