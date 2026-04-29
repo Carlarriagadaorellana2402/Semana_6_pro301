58980ea191d680d8c81454dacf9b705c41f60a8d
Carlarriagadaorellana2402 <carriagadaore@gmail.com>
2026-04-28 21:56:29 -0400

    Delete registro.php

--- registro.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="../controllers/UsuarioController.php">
-<input name="nombre" placeholder="Nombre">
-<input name="email" placeholder="Email">
-<input name="password" placeholder="Password">
-<button>Registrar</button>
-</form>
